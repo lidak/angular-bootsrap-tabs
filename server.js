9795bdd5ee71b2c0cace118dd62da2a42a3b1e4b
Oleksandr Lidak <Oleksandr Lidak>
2016-03-29 10:41:13 +0300

    server port changed.

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('*', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen('5000', function() {
+app.listen('8080', function() {
     console.log('Static was served by express.');
 });
